4c99d13f44324943b14128efdbe1758a9f9c2daa
Augusto Coria <coria.augusto.ram@gmail.com>
2024-09-04 22:54:22 -0300

    Update cadenas.py
    prueba de cosas

--- cadenas_python/cadenas.py
+++ cadenas_python/cadenas.py
@@ -1,1 +1,13 @@
-print("hola pibes como estan")
+
+
+
+def filtrarPalabras(oracion, combinaciones):
+    
+    palabras = oracion.split()
+
+    palabrasFiltradas = [palabra for palabra in palabras if all(comb not in palabra for comb in combinaciones)]
+
+    return ' '.join(palabrasFiltradas)
+
+
+print(filtrarPalabras("el establo esta cerrado", ["blo", "el"]))
